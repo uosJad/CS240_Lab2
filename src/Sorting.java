683f677e844cc08f23890f9727589390bb85d628
Jason Wu <wujason0@gmail.com>
2017-02-01 19:05:30 -0800

    added recursive selection sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -34,7 +34,7 @@ public class Sorting {
 		for (int i = 1; i < temp.length; i++){
 			least = i;
 			
-			for (int j = i; j < temp.length; j++){
+			for (int j = i+1; j < temp.length; j++){
 				// check all for least, swap least to i
 				if (arr[j] < least)
 					least = j;
@@ -45,6 +45,53 @@ public class Sorting {
 		
 		return temp;
 	}
+	
+	
+	/**
+	 * User access version of SelectiveSortRecursive
+	 * @return copy of sorted array
+	 */
+	public int[] SelectiveSortRecursive(){
+		return SelectiveSortRecursive(CopyArray(arr), 1, 1+1, -1);
+	}
+	
+	/**
+	 * Implementation of recursive selective sorting algorithm
+	 * @param tempArray array to sort
+	 * @param currentIndex starting index (i)
+	 * @param checkingIndex should always equal current index + 1 at start (j)
+	 * @param least records index with least value, -1 if starting to check for least
+	 * @return copy of sorted array
+	 */
+	private int[] SelectiveSortRecursive(int[] tempArray, int currentIndex, int checkingIndex, int least){
+		
+		if (currentIndex < tempArray.length) { // if current index less than length
+			if (least == -1){ //if on new current index, set least
+				least = currentIndex;
+			}
+			
+			if (checkingIndex < tempArray.length){ // if checking is less then length
+				if (tempArray[checkingIndex] < tempArray[currentIndex]) { //set checking to least if least
+					least = checkingIndex;
+				}
+				
+				//do recursive again with checking++
+				SelectiveSortRecursive(tempArray,  currentIndex, checkingIndex+1, least);
+				
+			}
+			else //if finished checking for least (j loop)
+			{
+				if (currentIndex != least)
+					swap(tempArray, currentIndex, least);
+				
+				//assign least to -1 to get reassigned
+				SelectiveSortRecursive(tempArray,  currentIndex+1, checkingIndex+2, -1);
+			}
+		}
+		
+		return tempArray; // executes once recursive stack completes	
+	}
+	
 	
 	/**
 	 * Swaps the values of the current array at index a and b
